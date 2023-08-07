859cd30a8f6522eebd019e7e38014d48e5bfa686
Shu <56845481+shuart-shuart@users.noreply.github.com>
2023-08-08 02:00:13 +0800

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -1,5 +1,5 @@
 <?php
-$url = "https://docs.google.com/spreadsheets/d/1B_bqGGqrtBrM8aNIaHsmsbIjeGA7KH3vet-DptWE0NM/gviz/tq?tqx=out:csv";
+$url = "https://docs.google.com/spreadsheets/d/1B_bqGGqrtBrM8aNIaHsmsbIjeGA7KH3vet-DptWE0NM/gviz/tq?tqx=out:csv&range=A2%3AB1000";
 
 $response = file_get_contents($url);
 
